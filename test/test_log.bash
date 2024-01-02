777c90233d7bb7ce9fcf71fe0c9eadeb5ae874e1
Siromi463 <kimimi.nyan@gmail.com>
2024-01-02 22:57:56 +0900

    add

--- test/test_log.bash
+++ test/test_log.bash
@@ -12,11 +12,3 @@ timeout 20 ros2 launch mypkg log_talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'Successfully wrote '10' to the file'
-
-# ファイルの内容をチェック
-if grep -q '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n' log.txt; then
-    echo "The file contents are correct."
-else
-    echo "The file contents are not correct."
-    exit 1
-fi
