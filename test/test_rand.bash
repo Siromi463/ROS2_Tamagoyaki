1fe7cee25c1b3bae85df46ac1903c48d279285fe
Siromi463 <kimimi.nyan@gmail.com>
2024-01-04 13:18:11 +0900

    add

---
+++ test/test_rand.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Siromi463 kimimi.nyan@gmail.com
+# SPDX-License-Identifier: BSD-3-Clause
+
+dir=~
+[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+cat /tmp/mypkg.log |
+grep -E 'Listen: (0|[1-9][0-9]?|100)'
